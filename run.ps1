cc99c7b6839132202636e0f625816b53f42234e1
abel1502 <terragame.abel@gmail.com>
2023-07-24 18:12:48 +0300

    Disable incremental build

--- run.ps1
+++ run.ps1
@@ -1,1 +1,2 @@
-bundle exec jekyll serve --future --draft --incremental
+# Note: --incremental messes with tailwindcss classes, at least in posts
+bundle exec jekyll serve --future --draft  # --incremental
